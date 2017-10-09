b9b8920461fa7384e8775d1f1e1aa84f90705137
James Devine <j.devine@lancaster.ac.uk>
2017-10-09 11:23:50 +0100

    moved USB endpoint definitions to CodalUSB.h
    These previously resided in target specific repos, but should be part of the high level interface.

--- inc/core/CodalUSB.h
+++ inc/core/CodalUSB.h
@@ -7,7 +7,6 @@
 
 #include <stdint.h>
 #include "ErrorNo.h"
-#include "USB.h"
 
 #define GET_STATUS 0
 #define CLEAR_FEATURE 1
@@ -178,6 +177,41 @@ typedef struct
 } __attribute__((packed)) StringDescriptor;
 
 #pragma GCC diagnostic ignored "-Wunused-parameter"
+
+class UsbEndpointIn
+{
+    uint8_t buf[USB_MAX_PKT_SIZE];
+
+public:
+    uint8_t ep;
+    uint8_t flags;
+    uint16_t wLength;
+    int stall();
+    int reset();
+    int write(const void *buf, int length);
+
+    UsbEndpointIn(uint8_t idx, uint8_t type, uint8_t size = USB_MAX_PKT_SIZE);
+};
+
+class UsbEndpointOut
+{
+    uint8_t buf[USB_MAX_PKT_SIZE];
+    void startRead();
+
+public:
+    uint8_t ep;
+    int stall();
+    int reset();
+
+    int read(void *buf, int maxlength); // up to packet size
+    // int readBlocking(const void *buf, int length);
+
+    UsbEndpointOut(uint8_t idx, uint8_t type, uint8_t size = USB_MAX_PKT_SIZE);
+};
+
+void usb_configure(uint8_t numEndpoints);
+void usb_set_address(uint16_t wValue);
+
 
 class CodalUSBInterface
 {
