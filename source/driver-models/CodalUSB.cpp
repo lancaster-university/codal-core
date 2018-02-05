86557e1397f2b440ab3df35b307d0c18f6363093
Michal Moskal <michal@moskal.me>
2018-02-05 14:55:59 -0800

    Set USB v2.10 when WebUSB

--- source/driver-models/CodalUSB.cpp
+++ source/driver-models/CodalUSB.cpp
@@ -48,7 +48,11 @@ static const ConfigDescriptor static_config = {9, 2, 0, 0, 1, 0, USB_CONFIG_BUS_
 static const DeviceDescriptor default_device_desc = {
     0x12,            // bLength
     0x01,            // bDescriptorType
+#if CONFIG_ENABLED(DEVICE_WEBUSB)
+    0x0210,          // bcdUSBL
+#else
     0x0200,          // bcdUSBL
+#endif
     0xEF,            // bDeviceClass:    Misc
     0x02,            // bDeviceSubclass:
     0x01,            // bDeviceProtocol:
