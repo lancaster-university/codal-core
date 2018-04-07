6ba024a6d32b28d8ccfe65b1313abb22b56c728a
Michal Moskal <michal@moskal.me>
2018-04-06 18:05:24 -0700

    Add basic screen driver

---
+++ inc/drivers/ST7735.h
@@ -0,0 +1,61 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2017 Lancaster University.
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef DEVICE_ST7735_H
+#define DEVICE_ST7735_H
+
+#include "Pin.h"
+#include "SPI.h"
+
+namespace codal
+{
+
+class ST7735
+{
+    SPI &spi;
+    Pin &cs;
+    Pin &dc;
+
+    void sendCmd(uint8_t *buf, int len);
+    void sendCmdSeq(const uint8_t *buf);
+
+public:
+    ST7735(SPI &spi, Pin &cs, Pin &dc);
+    void init();
+    void sendColors(const void *colors, int byteSize);
+    void setAddrWindow(int x, int y, int w, int h);
+};
+
+static inline uint16_t color565(uint8_t r, uint8_t g, uint8_t b)
+{
+    return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3);
+}
+
+static inline uint16_t color565(uint32_t rgb)
+{
+    return color565((rgb >> 16) & 0xff, (rgb >> 8) & 0xff, rgb & 0xff);
+}
+}
+
+#endif
---
+++ source/drivers/ST7735.cpp
@@ -0,0 +1,180 @@
+#include "ST7735.h"
+#include "CodalFiber.h"
+
+#define ST7735_NOP 0x00
+#define ST7735_SWRESET 0x01
+#define ST7735_RDDID 0x04
+#define ST7735_RDDST 0x09
+
+#define ST7735_SLPIN 0x10
+#define ST7735_SLPOUT 0x11
+#define ST7735_PTLON 0x12
+#define ST7735_NORON 0x13
+
+#define ST7735_INVOFF 0x20
+#define ST7735_INVON 0x21
+#define ST7735_DISPOFF 0x28
+#define ST7735_DISPON 0x29
+#define ST7735_CASET 0x2A
+#define ST7735_RASET 0x2B
+#define ST7735_RAMWR 0x2C
+#define ST7735_RAMRD 0x2E
+
+#define ST7735_PTLAR 0x30
+#define ST7735_COLMOD 0x3A
+#define ST7735_MADCTL 0x36
+
+#define ST7735_FRMCTR1 0xB1
+#define ST7735_FRMCTR2 0xB2
+#define ST7735_FRMCTR3 0xB3
+#define ST7735_INVCTR 0xB4
+#define ST7735_DISSET5 0xB6
+
+#define ST7735_PWCTR1 0xC0
+#define ST7735_PWCTR2 0xC1
+#define ST7735_PWCTR3 0xC2
+#define ST7735_PWCTR4 0xC3
+#define ST7735_PWCTR5 0xC4
+#define ST7735_VMCTR1 0xC5
+
+#define ST7735_RDID1 0xDA
+#define ST7735_RDID2 0xDB
+#define ST7735_RDID3 0xDC
+#define ST7735_RDID4 0xDD
+
+#define ST7735_PWCTR6 0xFC
+
+#define ST7735_GMCTRP1 0xE0
+#define ST7735_GMCTRN1 0xE1
+
+namespace codal
+{
+
+ST7735::ST7735(SPI &spi, Pin &cs, Pin &dc) : spi(spi), cs(cs), dc(dc) {}
+
+#define DELAY 0x80
+
+// clang-format off
+static const uint8_t initCmds[] = {
+    ST7735_SWRESET,   DELAY,  //  1: Software reset, 0 args, w/delay
+      120,                    //     150 ms delay
+    ST7735_SLPOUT ,   DELAY,  //  2: Out of sleep mode, 0 args, w/delay
+      120,                    //     500 ms delay
+    ST7735_FRMCTR1, 3      ,  //  3: Frame rate ctrl - normal mode, 3 args:
+      0x01, 0x2C, 0x2D,       //     Rate = fosc/(1x2+40) * (LINE+2C+2D)
+    ST7735_FRMCTR2, 3      ,  //  4: Frame rate control - idle mode, 3 args:
+      0x01, 0x2C, 0x2D,       //     Rate = fosc/(1x2+40) * (LINE+2C+2D)
+    ST7735_FRMCTR3, 6      ,  //  5: Frame rate ctrl - partial mode, 6 args:
+      0x01, 0x2C, 0x2D,       //     Dot inversion mode
+      0x01, 0x2C, 0x2D,       //     Line inversion mode
+    ST7735_INVCTR , 1      ,  //  6: Display inversion ctrl, 1 arg, no delay:
+      0x07,                   //     No inversion
+    ST7735_PWCTR1 , 3      ,  //  7: Power control, 3 args, no delay:
+      0xA2,
+      0x02,                   //     -4.6V
+      0x84,                   //     AUTO mode
+    ST7735_PWCTR2 , 1      ,  //  8: Power control, 1 arg, no delay:
+      0xC5,                   //     VGH25 = 2.4C VGSEL = -10 VGH = 3 * AVDD
+    ST7735_PWCTR3 , 2      ,  //  9: Power control, 2 args, no delay:
+      0x0A,                   //     Opamp current small
+      0x00,                   //     Boost frequency
+    ST7735_PWCTR4 , 2      ,  // 10: Power control, 2 args, no delay:
+      0x8A,                   //     BCLK/2, Opamp current small & Medium low
+      0x2A,  
+    ST7735_PWCTR5 , 2      ,  // 11: Power control, 2 args, no delay:
+      0x8A, 0xEE,
+    ST7735_VMCTR1 , 1      ,  // 12: Power control, 1 arg, no delay:
+      0x0E,
+    ST7735_INVOFF , 0      ,  // 13: Don't invert display, no args, no delay
+    ST7735_MADCTL , 1      ,  // 14: Memory access control (directions), 1 arg:
+      0xC8,                   //     row addr/col addr, bottom to top refresh
+    ST7735_COLMOD , 1      ,  // 15: set color mode, 1 arg, no delay:
+      0x05,                  //     16-bit color
+
+    ST7735_CASET  , 4      ,  //  1: Column addr set, 4 args, no delay:
+      0x00, 0x00,             //     XSTART = 0
+      0x00, 0x7F,             //     XEND = 127
+    ST7735_RASET  , 4      ,  //  2: Row addr set, 4 args, no delay:
+      0x00, 0x00,             //     XSTART = 0
+      0x00, 0x7F,           //     XEND = 127 --XXX160
+
+    ST7735_GMCTRP1, 16      , //  1: Magical unicorn dust, 16 args, no delay:
+      0x02, 0x1c, 0x07, 0x12,
+      0x37, 0x32, 0x29, 0x2d,
+      0x29, 0x25, 0x2B, 0x39,
+      0x00, 0x01, 0x03, 0x10,
+    ST7735_GMCTRN1, 16      , //  2: Sparkles and rainbows, 16 args, no delay:
+      0x03, 0x1d, 0x07, 0x06,
+      0x2E, 0x2C, 0x29, 0x2D,
+      0x2E, 0x2E, 0x37, 0x3F,
+      0x00, 0x00, 0x02, 0x10,
+    ST7735_NORON  ,    DELAY, //  3: Normal display on, no args, w/delay
+      10,                     //     10 ms delay
+    ST7735_DISPON ,    DELAY, //  4: Main screen turn on, no args w/delay
+      10,
+
+    0, 0 // END
+};
+// clang-format on
+
+// we don't modify *buf, but it cannot be in flash, so no const as a hint
+void ST7735::sendCmd(uint8_t *buf, int len)
+{
+    dc.setDigitalValue(0);
+    cs.setDigitalValue(0);
+    spi.write(*buf);
+    dc.setDigitalValue(1);
+    len--;
+    buf++;
+    if (len > 0)
+        spi.transfer(buf, len, NULL, 0);
+    cs.setDigitalValue(1);
+}
+
+void ST7735::sendCmdSeq(const uint8_t *buf)
+{
+    uint8_t tmp[20];
+    while (*buf)
+    {
+        tmp[0] = *buf++;
+        int v = *buf++;
+        int len = v & ~DELAY;
+        // note that we have to copy to RAM
+        memcpy(tmp + 1, buf, len);
+        sendCmd(tmp, len + 1);
+        buf += len;
+        if (v & DELAY)
+        {
+            fiber_sleep(*buf++);
+        }
+    }
+}
+
+void ST7735::setAddrWindow(int x, int y, int w, int h)
+{
+    uint8_t cmd0[] = {ST7735_CASET, 0, (uint8_t)x, 0, (uint8_t)(x + w - 1)};
+    uint8_t cmd1[] = {ST7735_RASET, 0, (uint8_t)y, 0, (uint8_t)(y + h - 1)};
+    sendCmd(cmd0, sizeof(cmd0));
+    sendCmd(cmd1, sizeof(cmd1));
+}
+
+void ST7735::sendColors(const void *colors, int byteSize)
+{
+    dc.setDigitalValue(1);
+    cs.setDigitalValue(0);
+    spi.transfer((const uint8_t*)colors, byteSize, NULL, 0);
+    cs.setDigitalValue(1);
+}
+
+void ST7735::init()
+{
+    spi.setFrequency(8000000);
+    spi.setMode(0);
+
+    cs.setDigitalValue(1);
+    dc.setDigitalValue(1);
+
+    fiber_sleep(10);
+    sendCmdSeq(initCmds);
+}
+}
