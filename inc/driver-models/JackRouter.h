11716df3244cdafbaa423236ced0ff8f03454e61
Michal Moskal <michal@moskal.me>
2018-06-27 17:24:59 -0700

    Add class for switching between network/audio/buzzer

---
+++ inc/driver-models/JackRouter.h
@@ -0,0 +1,83 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2018 Lancaster University.
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef CODAL_JACK_ROUTER_H
+#define CODAL_JACK_ROUTER_H
+
+#include "CodalConfig.h"
+#include "ErrorNo.h"
+#include "Pin.h"
+
+namespace codal
+{
+
+enum class JackState : uint8_t
+{
+    AllDown,
+    HeadPhones,
+    Buzzer,
+    BuzzerAndSerial,
+};
+
+/**
+ * Class definition for detector of plug/unplug audio jack events.
+ */
+class JackRouter : public CodalComponent
+{
+    bool floatUp;
+    bool lastSenseFloat;
+    uint8_t numIdles;
+    uint8_t numLows;
+    uint8_t numSenseForced;
+    JackState state;
+
+    Pin &tip;
+    Pin &sense;
+    Pin &hpEn;
+    Pin &bzEn;
+    Pin &pwrEn;
+
+    void setState(JackState s);
+    /**
+     * Check if the sense pin is floating
+     */
+    void checkFloat();
+
+public:
+    JackRouter(Pin &tip, Pin &sense, Pin &headphoneEnable, Pin &buzzerEnable, Pin &powerEnable);
+
+    /**
+     * Implement this function to receive a callback when the device is idling.
+     */
+    virtual void idleCallback();
+
+    /**
+     * Return the current state of the router.
+     */
+    JackState getState() { return state; }
+};
+
+} // namespace codal
+
+#endif
---
+++ source/driver-models/JackRouter.cpp
@@ -0,0 +1,152 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2018 Lancaster University.
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+*/
+
+#include "JackRouter.h"
+#include "ErrorNo.h"
+#include "CodalDmesg.h"
+
+#define MAX_IDLES 200
+
+namespace codal
+{
+
+JackRouter::JackRouter(Pin &tip, Pin &sense, Pin &headphoneEnable, Pin &buzzerEnable,
+                       Pin &powerEnable)
+    : tip(tip), sense(sense), hpEn(headphoneEnable), bzEn(buzzerEnable), pwrEn(powerEnable)
+{
+    status |= DEVICE_COMPONENT_STATUS_IDLE_TICK;
+
+    floatUp = false;
+    lastSenseFloat = false;
+    numIdles = 0;
+    numLows = 0;
+    numSenseForced = 0;
+
+    state = (JackState)-1;
+    setState(JackState::AllDown);
+}
+
+void JackRouter::setState(JackState s)
+{
+    if (state == s)
+        return;
+
+    if (state == JackState::BuzzerAndSerial)
+    {
+        // shut down serial
+    }
+
+    state = s;
+    hpEn.setDigitalValue(state == JackState::HeadPhones);
+    bzEn.setDigitalValue(state == JackState::Buzzer || state == JackState::BuzzerAndSerial);
+    pwrEn.setDigitalValue(state == JackState::BuzzerAndSerial);
+
+    DMESG("Jack plug-in state: %d", state);
+
+    if (state == JackState::BuzzerAndSerial)
+    {
+        // start serial
+    }
+}
+
+void JackRouter::checkFloat()
+{
+    // pull the sense up or down and see if it sticks
+    sense.setPull(floatUp ? PullMode::Up : PullMode::Down);
+    sense.getDigitalValue(); // let it stabilize
+    if (sense.getDigitalValue() != floatUp)
+    {
+        // if it didn't stick, it means sense is connected to something
+        // ie., the jack was unplugged
+        numSenseForced++;
+    }
+}
+
+/**
+ * Implement this function to receive a callback when the device is idling.
+ */
+void JackRouter::idleCallback()
+{
+    if (state == JackState::HeadPhones)
+    {
+        floatUp = !floatUp;
+        checkFloat();
+    }
+    else if (state == JackState::Buzzer || state == JackState::AllDown)
+    {
+        // in buzzer mode - try to turn on the power for a moment to see if sense is connected to it
+        pwrEn.setDigitalValue(1);
+        floatUp = false;
+        checkFloat();
+        pwrEn.setDigitalValue(0);
+    }
+    else if (state == JackState::BuzzerAndSerial)
+    {
+        // power is already on, just see is sense is connected to it
+        floatUp = false;
+        checkFloat();
+    }
+
+    if (state != JackState::BuzzerAndSerial)
+    {
+        if (!tip.getDigitalValue(PullMode::Up))
+            numLows++;
+    }
+
+    numIdles++;
+
+    if (numIdles < MAX_IDLES)
+        return;
+
+    //DMESG("cycle: lf=%d nl=%d nf=%d", lastSenseFloat, numLows, numSenseForced);
+
+    if (lastSenseFloat)
+    {
+        // something is connected - the sense was floating the previous cycle
+
+        if (numLows == MAX_IDLES)
+        {
+            // the TX was low the entire time
+            setState(JackState::HeadPhones);
+        }
+        else
+        {
+            // the TX was up some time - assume networking mode
+            setState(JackState::BuzzerAndSerial);
+        }
+    }
+    else
+    {
+        // nothing connected - just send sound to the buzzer
+        setState(JackState::Buzzer);
+    }
+
+    lastSenseFloat = numSenseForced == 0;
+
+    numIdles = 0;
+    numLows = 0;
+    numSenseForced = 0;
+}
+
+} // namespace codal
