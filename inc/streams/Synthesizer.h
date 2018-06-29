461ee68ecff0e92bf2b48e12f09586da7803dd03
Michal Moskal <michal@moskal.me>
2018-06-29 15:26:50 -0700

    Start on envelope in synth

--- inc/streams/Synthesizer.h
+++ inc/streams/Synthesizer.h
@@ -46,10 +46,12 @@ namespace codal
         int     amplitude;             // The maximum amplitude of the wave to generate (the volume of the output)
         bool    active;                // Determines if background playback of audio is currently active.
         bool    synchronous;           // Determines if a synchronous mode of operation has been requested.
+        bool    isSigned;              // If true, samples use int16_t otherwise uint16_t.
 
         ManagedBuffer buffer;          // Playout buffer.
         int     bytesWritten;          // Number of bytes written to the output buffer.
-        const uint16_t *tonePrint;     // The tone currently selected playout tone.
+        const uint16_t *tonePrint;     // The tone currently selected playout tone (always unsigned).
+        int     position;              // Position within the tonePrint
 
         public:
 
@@ -65,7 +67,7 @@ namespace codal
           *
           * @param sampleRate The sample rate at which this synthesizer will produce data.
           */
-        Synthesizer(int sampleRate = SYNTHESIZER_SAMPLE_RATE);
+        Synthesizer(int sampleRate = SYNTHESIZER_SAMPLE_RATE, bool isSigned = false);
 
         /**
           * Destructor.
@@ -75,16 +77,18 @@ namespace codal
 
         /**
         * Define the central frequency of this synthesizer. Takes effect at the start of the next waveform period.
-        * @frequency The frequency, in Hz to generate.
+        * @param frequency The frequency, in Hz to generate.
         */
         int setFrequency(float frequency);
 
         /**
         * Define the central frequency of this synthesizer. Takes effect at the start of the next waveform period.
-        * @frequency The frequency, in Hz to generate.
-        * @period The period, in ms, to play the frequency.
+        * @param frequency The frequency, in Hz to generate.
+        * @param period The period, in ms, to play the frequency.
+        * @param envelopeStart Starting amplitude (multiplied by general volume of this synth)
+        * @param envelopeEnd Final amplitude (multiplied by general volume of this synth)
         */
-        int setFrequency(float frequency, int period);
+        int setFrequency(float frequency, int period, int envelopeStart = 1024, int envelopeEnd = 1024);
 
         /**
         * Define the volume of the wave to generate.
@@ -126,7 +130,7 @@ namespace codal
         /**
         * Creates the next audio buffer, and attmepts to queue this on the output stream.
         */
-        void generate(int playoutTimeUs);
+        void generate(int playoutTimeUs, int envelopeStart = 1024, int envelopeEnd = 1024);
 
         /**
          * Defines the tone to generate.
