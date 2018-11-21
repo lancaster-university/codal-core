32f82587c0cf206536e2236b86dbcb3f26eb25a8
James Devine <j.devine@lancaster.ac.uk>
2018-11-21 17:50:57 +0000

    reserve ranges of addresses for makecode and codal

--- inc/JACDAC/JDClasses.h
+++ inc/JACDAC/JDClasses.h
@@ -1,15 +1,21 @@
 #ifndef JD_CLASSES_H
 #define JD_CLASSES_H
 
-#define JD_DRIVER_CLASS_CONTROL        0
-#define JD_DRIVER_CLASS_ARCADE         1
-#define JD_DRIVER_CLASS_JOYSTICK       2
-#define JD_DRIVER_CLASS_MESSAGE_BUS    3
-#define JD_DRIVER_CLASS_RADIO          4
-#define JD_DRIVER_CLASS_BRIDGE         5
-#define JD_DRIVER_CLASS_BUTTON         6
-#define JD_DRIVER_CLASS_PIN            7
-#define JD_DRIVER_CLASS_RELIABILITY_TESTER             8
-#define JD_DRIVER_CLASS_ACCELEROMETER                  9
+#define JD_DRIVER_CLASS_CODAL_START                     0
+#define JD_DRIVER_CLASS_CODAL_END                       2000
+
+#define JD_DRIVER_CLASS_CONTROL                         0
+#define JD_DRIVER_CLASS_ARCADE                          1
+#define JD_DRIVER_CLASS_JOYSTICK                        2
+#define JD_DRIVER_CLASS_MESSAGE_BUS                     3
+#define JD_DRIVER_CLASS_RADIO                           4
+#define JD_DRIVER_CLASS_BRIDGE                          5
+#define JD_DRIVER_CLASS_BUTTON                          6
+#define JD_DRIVER_CLASS_PIN                             7
+#define JD_DRIVER_CLASS_RELIABILITY_TESTER              8
+#define JD_DRIVER_CLASS_ACCELEROMETER                   9
+
+#define JD_DRIVER_CLASS_MAKECODE_START                  2000
+#define JD_DRIVER_CLASS_MAKECODE_END                    4000
 
 #endif
