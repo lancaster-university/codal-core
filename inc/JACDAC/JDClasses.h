02244f5ade59bd63f9d2254c64561207311c6247
James Devine <j.devine@lancaster.ac.uk>
2018-11-21 19:03:06 +0000

    removed unused drivers and class names

--- inc/JACDAC/JDClasses.h
+++ inc/JACDAC/JDClasses.h
@@ -3,19 +3,17 @@
 
 #define JD_DRIVER_CLASS_CODAL_START                     0
 #define JD_DRIVER_CLASS_CODAL_END                       2000
-
-#define JD_DRIVER_CLASS_CONTROL                         0
-#define JD_DRIVER_CLASS_ARCADE                          1
-#define JD_DRIVER_CLASS_JOYSTICK                        2
-#define JD_DRIVER_CLASS_MESSAGE_BUS                     3
-#define JD_DRIVER_CLASS_RADIO                           4
-#define JD_DRIVER_CLASS_BRIDGE                          5
-#define JD_DRIVER_CLASS_BUTTON                          6
-#define JD_DRIVER_CLASS_PIN                             7
-#define JD_DRIVER_CLASS_RELIABILITY_TESTER              8
-#define JD_DRIVER_CLASS_ACCELEROMETER                   9
-
 #define JD_DRIVER_CLASS_MAKECODE_START                  2000
 #define JD_DRIVER_CLASS_MAKECODE_END                    4000
+
+#define JD_DRIVER_CLASS_CONTROL                         0
+#define JD_DRIVER_CLASS_JOYSTICK                        1
+#define JD_DRIVER_CLASS_MESSAGE_BUS                     2
+#define JD_DRIVER_CLASS_BRIDGE                          3
+#define JD_DRIVER_CLASS_BUTTON                          4
+#define JD_DRIVER_CLASS_PIN                             5
+#define JD_DRIVER_CLASS_RELIABILITY_TESTER              6
+#define JD_DRIVER_CLASS_ACCELEROMETER                   7
+#define JD_DRIVER_CLASS_CAPTOUCH_BUTTON                 8
 
 #endif
