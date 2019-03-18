8c5308e6afff86cfc5c95ec09250e1d07dbe73af
James Devine <j.devine@lancaster.ac.uk>
2019-03-18 16:09:10 -0700

    rename service class constants

--- inc/JACDAC/JDClasses.h
+++ inc/JACDAC/JDClasses.h
@@ -6,19 +6,19 @@
 #define DYNAMIC_CLASS_START                             STATIC_ADDRESS_END
 #define DYNAMIC_CLASS_END                               0xFFFFFFFF
 
-#define JD_DRIVER_CLASS_CODAL_START                     0
-#define JD_DRIVER_CLASS_CODAL_END                       2000
-#define JD_DRIVER_CLASS_MAKECODE_START                  2000
-#define JD_DRIVER_CLASS_MAKECODE_END                    4000
+#define JD_SERVICE_CLASS_CODAL_START                     0
+#define JD_SERVICE_CLASS_CODAL_END                       2000
+#define JD_SERVICE_CLASS_MAKECODE_START                  2000
+#define JD_SERVICE_CLASS_MAKECODE_END                    4000
 
-#define JD_DRIVER_CLASS_CONTROL                         0
-#define JD_DRIVER_CLASS_JOYSTICK                        1
-#define JD_DRIVER_CLASS_MESSAGE_BUS                     2
-#define JD_DRIVER_CLASS_BRIDGE                          3
-#define JD_DRIVER_CLASS_BUTTON                          4
-#define JD_DRIVER_CLASS_PIN                             5
-#define JD_DRIVER_CLASS_RELIABILITY_TESTER              6
-#define JD_DRIVER_CLASS_ACCELEROMETER                   7
-#define JD_DRIVER_CLASS_CAPTOUCH_BUTTON                 8
+#define JD_SERVICE_CLASS_CONTROL                         0
+#define JD_SERVICE_CLASS_JOYSTICK                        1
+#define JD_SERVICE_CLASS_MESSAGE_BUS                     2
+#define JD_SERVICE_CLASS_BRIDGE                          3
+#define JD_SERVICE_CLASS_BUTTON                          4
+#define JD_SERVICE_CLASS_PIN                             5
+#define JD_SERVICE_CLASS_RELIABILITY_TESTER              6
+#define JD_SERVICE_CLASS_ACCELEROMETER                   7
+#define JD_SERVICE_CLASS_CAPTOUCH_BUTTON                 8
 
 #endif
