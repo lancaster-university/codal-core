688c38cedf2eb6e105679aada6e6eba77ed7fe02
James Devine <j.devine@lancaster.ac.uk>
2019-03-19 18:12:50 -0700

    Add JDPacketSniffer class

---
+++ inc/JACDAC/services/JDPacketSniffer.h
@@ -0,0 +1,31 @@
+#ifndef JD_PACKET_LOGGER_H
+#define JD_PACKET_LOGGER_H
+
+#include "JACDAC.h"
+#include "Event.h"
+
+namespace codal
+{
+
+    struct JDSnifferServiceMap
+    {
+        uint8_t device_address;
+        uint32_t services[JD_DEVICE_MAX_HOST_SERVICES];
+    };
+
+    class JDPacketSniffer: public JDService
+    {
+        JDDeviceManager deviceManager;
+
+        void timerCallback(Event);
+
+        public:
+        JDPacketSniffer();
+
+        virtual int handlePacket(JDPacket* p) override;
+
+        void logDevices();
+    };
+}
+
+#endif
