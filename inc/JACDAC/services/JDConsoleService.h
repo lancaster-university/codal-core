0431cf502dad5fafc8fad0ad28c2cab3a4953a68
James Devine <j.devine@lancaster.ac.uk>
2019-04-03 17:47:50 -0700

    Add console service.

---
+++ inc/JACDAC/services/JDConsoleService.h
@@ -0,0 +1,41 @@
+#ifndef JD_CONSOLE_SERVICE_H
+#define JD_CONSOLE_SERVICE_H
+
+#include "JACDAC.h"
+#include "ManagedString.h"
+
+#define JD_CONSOLE_LOG_PRIORITY_LOG         0
+#define JD_CONSOLE_LOG_PRIORITY_ERROR       1
+#define JD_CONSOLE_LOG_PRIORITY_WARNING     2
+#define JD_CONSOLE_LOG_PRIORITY_DEBUG       3
+
+#define JD_CONSOLE_LOG_STATUS_RECEIVER      0x02
+
+namespace codal
+{
+    enum JDConsoleLogPriority
+    {
+        Log = JD_CONSOLE_LOG_PRIORITY_LOG,
+        Error = JD_CONSOLE_LOG_PRIORITY_ERROR,
+        Warning = JD_CONSOLE_LOG_PRIORITY_WARNING,
+        Debug = JD_CONSOLE_LOG_PRIORITY_DEBUG
+    };
+
+    struct JDConsolePacket
+    {
+        uint8_t priority;
+        uint8_t message[]; // no size is needed, null terminator is included in the message.
+    };
+
+    class JDConsoleService : public JDService
+    {
+        public:
+        JDConsoleService(bool receiver);
+
+        int handlePacket(JDPacket* pkt) override;
+
+        int log(JDConsoleLogPriority priority, ManagedString message);
+    };
+}
+
+#endif
---
+++ source/JACDAC/services/JDConsoleService.cpp
@@ -0,0 +1,76 @@
+#include "JDConsoleService.h"
+#include "CodalDmesg.h"
+
+using namespace codal;
+
+JDConsoleService::JDConsoleService(bool receiver) :
+    JDService(JD_SERVICE_CLASS_CONSOLE, (receiver) ? BroadcastHostService : HostService)
+{
+    status = 0;
+
+    if (receiver)
+        status |= JD_CONSOLE_LOG_STATUS_RECEIVER;
+}
+
+int JDConsoleService::handlePacket(JDPacket* pkt)
+{
+    if (status & JD_CONSOLE_LOG_STATUS_RECEIVER)
+    {
+        JDConsolePacket* consolePkt =  (JDConsolePacket*)pkt->data;
+        char* priorityMsg = (char *)"";
+
+        switch ((JDConsoleLogPriority)consolePkt->priority)
+        {
+            case Log:
+                priorityMsg = (char *)"log";
+                break;
+
+            case Error:
+                priorityMsg = (char *)"error";
+                break;
+
+            case Warning:
+                priorityMsg = (char *)"warning";
+                break;
+
+            case Debug:
+                priorityMsg = (char *)"debug";
+                break;
+        }
+
+        JDDevice* device = JACDAC::instance->getRemoteDevice(pkt->device_address);
+
+        char* deviceName = (char*)"UNKNOWN";
+
+        if (device)
+        {
+            if (device->name)
+                deviceName = (char *)device->name;
+            else
+                deviceName = (char*)"UNNAMED";
+        }
+
+        DMESG("[%d,%s] %s: %s", priorityMsg, pkt->device_address, deviceName, consolePkt->message);
+    }
+
+    return DEVICE_OK;
+}
+
+int JDConsoleService::log(JDConsoleLogPriority priority, ManagedString message)
+{
+    // one for priority, the other for null terminator
+    if (message.length() + 2 > JD_SERIAL_MAX_PAYLOAD_SIZE || message.length() == 0)
+        return DEVICE_INVALID_PARAMETER;
+
+    // one for priority, the other for null terminator
+    JDConsolePacket* console = (JDConsolePacket*)malloc(message.length() + 2);
+
+    console->priority = priority;
+    memcpy(console->message, message.toCharArray(), message.length() + 1);
+
+    send((uint8_t*) console, message.length() + 2);
+
+    free(console);
+
+    return DEVICE_OK;
+}
