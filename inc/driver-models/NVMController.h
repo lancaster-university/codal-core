63c19944accd1db88247633ecd05ae9bed7f7100
James Devine <j.devine@lancaster.ac.uk>
2019-04-24 09:53:06 -0700

    Add default implementation of NVMController
    It does nothing, but is not an abstract class so it can be used.

--- inc/driver-models/NVMController.h
+++ inc/driver-models/NVMController.h
@@ -2,6 +2,7 @@
 #define CODAL_NVM_CTRL_H
 
 #include "CodalConfig.h"
+#include "ErrorNo.h"
 
 namespace codal
 {
@@ -9,19 +10,42 @@ namespace codal
     {
         public:
 
-        virtual uint32_t* getFlashEnd() = 0;
+        NVMController(){};
 
-        virtual uint32_t* getFlashStart() = 0;
+        virtual uint32_t* getFlashEnd()
+        {
+            return NULL;
+        }
 
-        virtual uint32_t getPageSize() = 0;
+        virtual uint32_t* getFlashStart()
+        {
+            return NULL;
+        }
 
-        virtual uint32_t getFlashSize() = 0;
+        virtual uint32_t getPageSize()
+        {
+            return 0;
+        }
 
-        virtual int copy(uint32_t* dest, uint32_t* source, uint32_t size) = 0;
+        virtual uint32_t getFlashSize()
+        {
+            return 0;
+        }
 
-        virtual int erase(uint32_t* page) = 0;
+        virtual int copy(uint32_t* dest, uint32_t* source, uint32_t size)
+        {
+            return DEVICE_NOT_IMPLEMENTED;
+        }
 
-        virtual int write(uint32_t* dst, uint32_t* source, uint32_t size) = 0;
+        virtual int erase(uint32_t* page)
+        {
+            return DEVICE_NOT_IMPLEMENTED;
+        }
+
+        virtual int write(uint32_t* dst, uint32_t* source, uint32_t size)
+        {
+            return DEVICE_NOT_IMPLEMENTED;
+        }
     };
 }
 
