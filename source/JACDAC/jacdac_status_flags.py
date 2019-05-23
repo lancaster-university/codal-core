68f0561d155d38f49e54bb3133b7cbea5d5d43f3
James Devine <j.devine@lancaster.ac.uk>
2019-05-23 09:28:19 -0700

    update constants in jacdac flag script

--- source/JACDAC/jacdac_status_flags.py
+++ source/JACDAC/jacdac_status_flags.py
@@ -2,10 +2,11 @@ import sys
 
 def process_flags(num):
     flags = {
-        "JD_SERIAL_RECEIVING": 0x0002,
-        "JD_SERIAL_RECEIVING_HEADER": 0x0004,
-        "JD_SERIAL_TRANSMITTING": 0x0008,
-        "JD_SERIAL_TX_DRAIN_ENABLE": 0x0010,
+        "JD_SERIAL_RECEIVING": 0x0001,
+        "JD_SERIAL_RECEIVING_HEADER": 0x0002,
+        "JD_SERIAL_TRANSMITTING": 0x0004,
+        "JD_SERIAL_RX_LO_PULSE": 0x0008,
+        "JD_SERIAL_TX_LO_PULSE": 0x0010,
 
         "JD_SERIAL_BUS_LO_ERROR": 0x0020,
         "JD_SERIAL_BUS_TIMEOUT_ERROR": 0x0040,
@@ -14,9 +15,9 @@ def process_flags(num):
 
         "JD_SERIAL_BUS_STATE": 0x0100,
         "JD_SERIAL_BUS_TOGGLED": 0x0200,
-        "JD_SERIAL_LO_PULSE_START": 0x0400,
 
-        "DEVICE_COMPONENT_RUNNING": 0x1000
+        "DEVICE_COMPONENT_RUNNING": 0x1000,
+        "JD_SERIAL_DEBUG_BIT": 0x8000
     }
 
     for flag in flags.keys():
