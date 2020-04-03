572af26c7e260c07bc0814c4a7d8350f6045e3f2
Michal Moskal <michal@moskal.me>
2020-04-03 08:30:58 -0700

    Use #define for WEAK

--- source/core/codal_default_target_hal.cpp
+++ source/core/codal_default_target_hal.cpp
@@ -3,27 +3,29 @@
 #include "CodalCompat.h"
 #include "Timer.h"
 
-__attribute__((weak)) void target_wait(uint32_t milliseconds)
+#define WEAK __attribute__((weak))
+
+WEAK void target_wait(uint32_t milliseconds)
 {
     codal::system_timer_wait_ms(milliseconds);
 }
 
-__attribute__((weak)) void target_wait_us(uint32_t us)
+WEAK void target_wait_us(uint32_t us)
 {
     codal::system_timer_wait_us(us);
 }
 
-__attribute__((weak)) int target_seed_random(uint32_t rand)
+WEAK int target_seed_random(uint32_t rand)
 {
     return codal::seed_random(rand);
 }
 
-__attribute__((weak)) int target_random(int max)
+WEAK int target_random(int max)
 {
     return codal::random(max);
 }
 
-__attribute__((weak)) void target_panic(int statusCode)
+WEAK void target_panic(int statusCode)
 {
     target_disable_irq();
 
@@ -33,7 +35,7 @@ __attribute__((weak)) void target_panic(int statusCode)
     }
 }
 
-__attribute__((weak)) void target_deepsleep()
+WEAK void target_deepsleep()
 {
     // if not implemented, default to WFI
     target_wait_for_event();
