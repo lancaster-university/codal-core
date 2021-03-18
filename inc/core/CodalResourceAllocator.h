330c61adf059c1c5fc4e60befd7be086d6c95ce2
James Devine <t-jamdev@microsoft.com>
2021-03-18 19:16:30 +0000

    further updates

--- inc/core/CodalResourceAllocator.h
+++ inc/core/CodalResourceAllocator.h
@@ -1,6 +1,28 @@
 #ifndef CODAL_RESOURCE_ALLOCATOR
 #define CODAL_RESOURCE_ALLOCATOR
 
+// it is typical of this peripheral to be subdivided (i.e. it has multiple "channels")
+#define PERIPHERAL_ATTR_SHARED                      0x01
+// this peripheral is dma capable
+#define PERIPHERAL_ATTR_DMA_CAPABLE                 0x02
+// this peripheral is capable of commanding devices (e.g. I2C/SPI controller)
+#define PERIPHERAL_ATTR_CONTROLLER_CAPABLE                 0x04
+// this peripheral is capable of being commanded by other devices (e.g. I2C/SPI device)
+#define PERIPHERAL_ATTR_SUBORDINATE_CAPABLE         0x08
+
+// different pieces of software can access this peripheral (e.g I2C)
+#define PERIPHERAL_PERMISSION_MULTIPLE_ACCESS       0x200
+#define PERIPHERAL_PERMISSION_SINGULAR_ACCESS       0x400
+
+enum PeripheralType {
+    PERIPHERAL_TYPE_I2C = 0x01,
+    PERIPHERAL_TYPE_SPI = 0x01,
+    PERIPHERAL_TYPE_PPI = 0x01,
+    PERIPHERAL_TYPE_PWM = 0x01,
+    PERIPHERAL_TYPE_UART = 0x01,
+    PERIPHERAL_TYPE_I2C = 0x01,
+}
+
 /**
   * Class definition for the CodalResourceAllocator.
   *
@@ -26,10 +48,11 @@ namespace codal
          * Records the given peripheral as allocated, if not already.
          * 
          * @param peripheral a pointer to the required peripheral
+         * @param driver a pointer to the driver instance that intends to command the peripheral
          * 
          * @return DEVICE_OK on success or DEVICE_NO_RESOURCES if already allocated.
          **/
-        int allocatePeripheral(void* peripheral);
+        int allocatePeripheral(void* peripheral, void* driver);
 
         /**
          * Records a subset of the given peripheral as allocated.
@@ -38,12 +61,13 @@ namespace codal
          * to mark a subset of a peripheral as in use.
          * 
          * @param peripheral a pointer to the required peripheral
+         * @param driver a pointer to the driver instance that intends to command the peripheral
          * @param rangeStart channel index range start
          * @param peripheral channel index range end
          * 
-         * @return handle on success or DEVICE_NO_RESOURCES if there is a collision.
+         * @return DEVICE_OK success or DEVICE_NO_RESOURCES if there is a collision.
          **/
-        int allocateSharedPeripheral(void* peripheral, int rangeStart, int rangeEnd);
+        int allocateSharedPeripheral(void* peripheral, void* driver, int rangeStart, int rangeEnd);
 
         /**
          * Records the given peripheral as free for other software drivers to use.
@@ -68,7 +92,7 @@ namespace codal
          * 
          * @return DEVICE_OK on success, DEVICE_INVALID_PARAMETER if the handle is not found.
          **/
-        int freeSharedPeripheral(void* peripheral, int handle);
+        int freeSharedPeripheral(void* peripheral, void* driver);
 
         /**
          * Returns the IRQ number for the given peripheral.
@@ -90,6 +114,16 @@ namespace codal
          **/
         int getIRQn(void* peripheral);
         
+        /**
+         * Retrieves a pointer to the CODAL driver commanding the given peripheral.
+         * 
+         * @param peripheral a pointer to the required peripheral
+         * 
+         * @return a pointer to the driver in control of the given peripheral. NULL if
+         *         none is found.
+         **/
+        void* obtainDriver(void* peripheral);
+
     };
 }
 
