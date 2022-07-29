cb902957e2ec942b8a90ddac1e5b677b03805cb6
John Vidler <j.vidler@lancaster.ac.uk>
2022-07-29 13:34:56 +0100

    Reverted to the break behaviour to preserve the tx interrupt

--- source/driver-models/Serial.cpp
+++ source/driver-models/Serial.cpp
@@ -97,8 +97,7 @@ int Serial::setTxInterrupt(uint8_t *string, int len, SerialMode mode)
                 while(txBufferedSize() > 0);
 
             if(mode == ASYNC)
-                return copiedBytes; // Explicitly return less than the full string. We can't 'immediately return' if we have to wait for more buffer space.
-                                    // Note that for applications that need to guarantee this works every time, they should set the buffer length before calling send().
+                break;
         }
 
         this->txBuff[txBuffHead] = string[copiedBytes];
