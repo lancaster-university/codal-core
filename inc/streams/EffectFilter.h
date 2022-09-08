f3320cae45c85cd05ceb0c908f23feda25dcc1dd
Joe Finney <joe@comp.lancs.ac.uk>
2022-09-08 18:02:12 +0100

    Refactor StreamFilter class
      - Replace WIP monolithic StreamFilter class with EffectFilter base class
      - Introduce explicit LowPassFilter component
      - Introduce deepCopy option to optionally decouple ManagedBuffers using streams APIs

---
+++ inc/streams/EffectFilter.h
@@ -0,0 +1,48 @@
+#include "ManagedBuffer.h"
+#include "DataStream.h"
+#include "StreamNormalizer.h"
+
+#ifndef EFFECT_FILTER_H
+#define EFFECT_FILTER_H
+
+namespace codal
+{
+    class EffectFilter : public DataSource, public DataSink
+    {
+        protected:
+
+        DataSink *downStream;
+        DataSource &upStream;
+        bool deepCopy;
+
+        public:
+
+        EffectFilter(DataSource &source, bool deepCopy = true);
+        ~EffectFilter();
+
+        virtual ManagedBuffer pull();
+        virtual int pullRequest();
+    	virtual void connect( DataSink &sink );
+        virtual void disconnect();
+        virtual int getFormat();
+        virtual int setFormat( int format );
+        
+        /**
+        * Defines if this filter should perform a deep copy of incoming data, or update data in place.
+        *
+        * @param deepCopy Set to true to copy incoming data into a freshly allocated buffer, or false to change data in place.
+        */
+        void setDeepCopy(bool deepCopy);
+
+        /**
+        * Default effect - a simple pass through filter. Override this method in subclasses to create specialist effects/filters.
+        * 
+        * @param inputBuffer the buffer containing data to process.
+        * @param outputBuffer the buffer in which to store the filtered data. n.b. MAY be the same memory as the input buffer.
+        * @param format the format of the data (word size and signed/unsigned representation)
+        */
+        virtual void applyEffect(ManagedBuffer inputBuffer, ManagedBuffer outputBuffer, int format);
+    };
+}
+
+#endif
---
+++ source/streams/EffectFilter.cpp
@@ -0,0 +1,78 @@
+#include "EffectFilter.h"
+#include "ManagedBuffer.h"
+#include "DataStream.h"
+#include "StreamNormalizer.h"
+#include "CodalDmesg.h"
+
+using namespace codal;
+
+EffectFilter::EffectFilter(DataSource &source, bool deepCopy) : upStream( source )
+{
+    this->downStream = NULL;
+    this->deepCopy = deepCopy;
+    source.connect( *this );
+}
+
+EffectFilter::~EffectFilter()
+{
+}
+
+ManagedBuffer EffectFilter::pull()
+{
+    ManagedBuffer input = this->upStream.pull();
+    ManagedBuffer output = deepCopy ? ManagedBuffer(input.length()) : input;
+
+    applyEffect(input, output, this->upStream.getFormat());
+    return output;
+}
+
+int EffectFilter::pullRequest()
+{
+    if( this->downStream != NULL )
+        this->downStream->pullRequest();
+
+    return 0;
+}
+
+void EffectFilter::connect(DataSink &sink)
+{
+    this->downStream = &sink;
+}
+
+void EffectFilter::disconnect()
+{
+    this->downStream = NULL;
+}
+
+int EffectFilter::getFormat()
+{
+    return this->upStream.getFormat();
+}
+
+int EffectFilter::setFormat( int format )
+{
+    return this->upStream.setFormat( format );
+}
+
+/**
+ * Defines if this filter should perform a deep copy of incoming data, or update data in place.
+ *
+ * @param deepCopy Set to true to copy incoming data into a freshly allocated buffer, or false to change data in place.
+ */
+void EffectFilter::setDeepCopy( bool deepCopy )
+{
+    this->deepCopy = deepCopy;
+}
+
+/**
+ * Default effect - a simple pass through filter.
+ * 
+ * @param inputBuffer the buffer containing data to process.
+ * @param outputBuffer the buffer in which to store the filtered data. n.b. MAY be the same memory as the input buffer.
+ * @param format the format of the data (word size and signed/unsigned representation)
+ */
+void EffectFilter::applyEffect(ManagedBuffer inputBuffer, ManagedBuffer outputBuffer, int format)
+{
+    if (inputBuffer.getBytes() != outputBuffer.getBytes())
+        memcpy(outputBuffer.getBytes(), inputBuffer.getBytes(), inputBuffer.length());
+}
