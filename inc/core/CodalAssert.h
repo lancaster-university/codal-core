4d477f1d5ee512fdb9083df4e1903d6a07a6e774
Dr John Vidler <j.vidler@lancaster.ac.uk>
2023-07-20 13:31:40 +0100

    Refactored assert to remove assert_true and assert_false in favour of a single unified assert call

--- inc/core/CodalAssert.h
+++ inc/core/CodalAssert.h
@@ -1,22 +1,30 @@
 #include "CodalConfig.h"
 #include "CodalDmesg.h"
 
+/**
+ * Intended for use in test suites - should not be used in production code!
+ * 
+ * (although they will compile to nothing if assertions are disabled...)
+ */
+
 #ifndef CODAL_ASSERT_H
 #define CODAL_ASSERT_H
 
 #ifndef __FILENAME__
-#define __FILENAME__ (__builtin_strrchr("/"__FILE__, '/') + 1)
+#define __FILENAME__ (__builtin_strrchr("/" __FILE__, '/') + 1)
 #endif
 
 #if CONFIG_ENABLED(CODAL_ENABLE_ASSERT)
-    void __codal_assert__( const char * file, const int line, bool condition, const char * expr, const char * expect, const char * message );
-    #define assert_true(condition, message) __codal_assert__( __FILENAME__, __LINE__, (condition) == true, #condition, "true", message )
-    #define assert_false(condition, message) __codal_assert__( __FILENAME__, __LINE__, (condition) == false, #condition, "false", message )
-    #define assert_never(message) __codal_assert__( __FILENAME__, __LINE__, false, "assert", "not executed", message )
+    void __codal_assert__( const char * file, const int line, bool condition, const char * expr, const char * message );
+    void __codal_assert_pass__( const char * file, const int line, const char * message );
+    void __codal_assert_fail__( const char * file, const int line, const char * message );
+    #define assert(condition, message) __codal_assert__( __FILENAME__, __LINE__, (condition) == true, #condition, message )
+    #define assert_pass(message) __codal_assert_pass__( __FILENAME__, __LINE__, message )
+    #define assert_fail(message) __codal_assert_fail__( __FILENAME__, __LINE__, message )
 #else
-    #define assert_true(x,y)
-    #define assert_false(x,y)
-    #define assert_never(x)
+    #define assert(x,y)
+    #define assert_pass(message)
+    #define assert_fail(message)
 #endif
 
 #endif
--- source/core/CodalAssert.cpp
+++ source/core/CodalAssert.cpp
@@ -2,17 +2,45 @@
 #include "CodalAssert.h"
 #include "CodalDmesg.h"
 
-uint32_t failures = 0;
+uint32_t test_failures = 0;
+uint32_t test_successes = 0;
 
-void __codal_assert__( const char * file, const int line, bool condition, const char * expr, const char * expect, const char * message ) {
-    if( condition )
+void __codal_assert__( const char * file, const int line, bool condition, const char * expr, const char * message ) {
+    DMESGN( "%s:%d; %s -> ", file, line, expr );
+
+    if( condition ) {
+        DMESG( "PASS" );
+        test_successes++;
         return;
-    DMESGF( "Assertion failure in %s line %d", file, line );
-    DMESGF( "\tThe expression (%s) did not evaluate to (%s)", expr, expect );
-    DMESGF( "\t%s", message );
+    }
+    DMESG( "FAIL" );
+    if( message != NULL )
+        DMESG( "\t%s", message );
+    
     #if CONFIG_ENABLED(CODAL_ASSERT_PANIC)
         target_panic( 999 );
     #else
-        DMESGF( "\tFailed %d tests", ++failures );
+        DMESG( "\tFailed %d tests", ++test_failures );
+    #endif
+}
+
+void __codal_assert_pass__( const char * file, const int line, const char * message ) {
+    test_successes++;
+
+    DMESG( "Test SUCCESS in %s:%d (PASS: %d, FAIL: %d)", file, line, test_successes, test_failures );
+    if( message != NULL )
+        DMESG( "\t%s", message );
+}
+
+void __codal_assert_fail__( const char * file, const int line, const char * message ) {
+    DMESG( "Test FAIL in %s:%d (PASS: %d, FAIL: %d)", file, line, test_successes, test_failures );
+    
+    if( message != NULL )
+        DMESG( "\t%s", message );
+    
+    #if CONFIG_ENABLED(CODAL_ASSERT_PANIC)
+        target_panic( 999 );
+    #else
+        DMESG( "\tFailed %d tests", ++test_failures );
     #endif
 }
